3979e6756ca450854e48947d91ff6957001f70fd
Dominik <hi@domthedev.com>
2022-01-25 15:14:07 +0100

    add final version

--- src/navi.js
+++ src/navi.js
@@ -1,15 +1,4 @@
-const navi = document.getElementById("navi");
-const bar1 = document.getElementById("bar1");
-const bar2 = document.getElementById("bar2");
-const bar3 = document.getElementById("bar3");
-
-document.getElementById("burger").addEventListener("click", function() {
-    // hide/show mobile nav
-    navi.classList.toggle("-right-full")
-    navi.classList.toggle("right-0")
-
-    // burger to cross
-    bar1.classList.toggle("rotate-45")
-    bar2.classList.toggle("hidden")
-    bar3.classList.toggle("-rotate-45")
+document.getElementById("toggle").addEventListener("click", function () {
+    document.getElementById("nav").classList.toggle("-right-full")
+    document.getElementById("nav").classList.toggle("right-0")
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,6 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    colors: {
-      brand: "#45C2B7",
-      dark: "#164E63",
-      mid: "#BEC9DF",
-      light: "#FFFFFF",
-      black: colors.black,
-      white: colors.white
-    },
     extend: {},
   },
   plugins: [],
